1c06c740070fd642daea21c557ab5129b371eda6
aps124 <aps124@ic.ac.uk>
2024-10-18 17:07:19 +0100

    Adding sort_data and split_data functionality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
 from binary_node import Binarytree
 
 
@@ -18,18 +19,47 @@ def load_data(filename):
                - y is a numpy array with shape (N, ), and each element should be 
                    an integer representing room number from 0 to C-1 where C
                    is the number of classes
-               - classes : a numpy array with shape (C, ), which contains the 
-                   unique class labels corresponding to the integers in y
     """
 
     X = np.loadtxt(filename, dtype=np.float64, usecols=(0,1,2,3,4,5,6))
     Y = np.loadtxt(filename, usecols=(7)).astype(np.int64)
-    # classes = np.unique(Y)
     return X, Y
 
 
-def split_data_train_test_cv(X, Y):
-    pass
+def train_test_split(X, Y, test_proportion, random_seed):
+    """ Split data X and Y into random train and test subsets according
+    to specified test_size proportion
+
+    Args:
+        X (np.array): Feature data
+        Y (np.array): Label data
+        test_proportion (float): Indicates proportion of dataset to be
+            test data. Must be between 0.0 and 1.0
+        random_seed (int): Seed used to initialize random generator
+
+    Returns:
+        - X_train (np.array): X data to use for train
+        - Y_train (np.array): Y data to use for train
+        - X_test (np.array): X data to use for test
+        - Y_test (np.array): Y data to use for test
+    """
+
+    # Create random generator
+    random_generator = np.random.default_rng(random_seed)
+
+    # Create array of shuffled indices
+    shuffled_indices = random_generator.permutation(len(X))
+
+    # Define size of test and train arrays based on input proportion
+    num_test = round(len(X) * test_proportion)
+
+    X_test = X[shuffled_indices[:num_test]]
+    Y_test = Y[shuffled_indices[:num_test]]
+
+    X_train = X[shuffled_indices[num_test:]]
+    Y_train = Y[shuffled_indices[num_test:]]
+
+    return X_train, Y_train, X_test, Y_test
 
 
 def create_node(attribute, value, left, right, leaf=False):
@@ -106,24 +136,52 @@ def find_split(X, Y):
     for feature in range(X.shape[1]):
 
         # Sort X by feature
-        Xsorted, Ysorted = sort(X, Y, feature). # TODO: implement sort
+        X_sorted, Y_sorted = sort_data(X, Y, feature)
 
-        midpoints = get_midpoints(Xsorted, feature) # TODO: implement get_midpoints
+        midpoints = get_midpoints(X_sorted, feature) # TODO: implement get_midpoints
         for midpoint in midpoints:
             # split data into two groups based on split point
-            Xleft, Yleft, Xright, Yright = split_data(Xsorted, Ysorted, feature, midpoint)
+            Xleft, Yleft, Xright, Yright = split_data(X_sorted, Y_sorted, feature, midpoint)
             # calculate information gain for each split point (feature data < value)
-            split_point_ig = calculate_information_gain(Ysorted, np.array(Yleft, Yright))
+            split_point_ig = calculate_information_gain(Y_sorted, np.array(Yleft, Yright))
             if split_point_ig > max_info_gain:
                 max_info_gain = split_point_ig
                 best_feature = feature
                 best_value = midpoint
-    
+
     return best_feature, best_value
 
 
-def sort_data():
-    pass
+def sort_data(X, Y, feature):
+    """ Sort data X, Y based on the feature column of X
+
+    Args:
+        X (np.array): Feature data
+        Y (np.array): Label data
+        feature (int): Index of feature column to sort on
+
+    Returns:
+        - X_sorted (np.array): X sorted on X[feature]
+        - Y_sorted (np.array): Y sorted on X[feature]
+    """
+
+    # Ensure feature is a valid column
+    if feature not in range(X.shape[1]):
+        raise ValueError(f"Feature {feature} does not exist in X")
+
+    # Reshape Y so that it can be joined with X
+    Y = Y.reshape(-1, 1)
+    # Combine Y with X so that it gets sorted alongside it
+    dataset = np.concatenate((X, Y), axis=1)
+
+    # Use argsort to sort data on feature
+    indices_sorted = np.argsort(dataset[:,feature])
+    dataset_sorted = dataset[indices_sorted]
+
+    # Separate dataset back into X and Y
+    X_sorted = dataset_sorted[:, :-1]
+    Y_sorted = dataset_sorted[:, -1]
+    return X_sorted, Y_sorted
 
 
 def get_midpoints():
@@ -160,9 +218,10 @@ def decision_tree_learning(X, Y, depth=0, max_depth=None):
     if depth == max_depth:
         return
 
+    # TODO: fix this bit
     # If all data in dataset has the same label, create leaf node
-    if all(Y) is the same:
-        return create_node(None, None, None, None, leaf_val=whatever Y is)
+    # if all(Y) is the same:
+    #     return create_node(None, None, None, None, leaf_val=whatever Y is)
 
     split_attribute, split_value = find_split(X, Y)
     Xleft, Yleft, Xright, Yright = split_data(X, Y, split_attribute, split_value)
@@ -200,4 +259,16 @@ def prune_tree():
 
 
 if __name__ == "__main__":
-    pass
+    clean_filename = 'wifi_db/clean_dataset.txt'
+    noisy_filename = 'wifi_db/noisy_dataset.txt'
+
+    X, Y = load_data(clean_filename)
+
+    X_train, Y_train, X_, Y_ = train_test_split(X, Y, 0.2, 42)
+    X_test, Y_test, X_cv, Y_cv = train_test_split(X_, Y_, 0.5, 42)
+
+    print(X_train.shape)
+    print(X_test.shape)
+    print(X_cv.shape)
+
+    X_sorted, Y_sorted = sort_data(X_cv, Y_cv, 5)
