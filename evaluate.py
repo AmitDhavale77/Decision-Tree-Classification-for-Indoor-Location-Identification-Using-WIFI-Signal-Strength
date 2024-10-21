bf9ac8735f31861b1c78b453c83d33a4dd2e8e44
aps124 <aanish.sikora24@imperial.ac.uk>
2024-10-21 09:10:14 +0000

    Update file evaluate.py

---
+++ evaluate.py
@@ -0,0 +1,18 @@
+# Do evaluation stuff
+
+
+def evaluation(y_predicted, y_true):
+    """
+    Confusion Matrix (4x4 matrix)
+    Then for each label:
+        Accuracy
+        Precision/Recall
+        F1 score
+    Then do macro-averaging to get overall
+    Should we do micro-averaging??? TBD
+    """
+
+    # Ideally this should return a single metric we can make decision on
+    # Do the math
+    return
+
