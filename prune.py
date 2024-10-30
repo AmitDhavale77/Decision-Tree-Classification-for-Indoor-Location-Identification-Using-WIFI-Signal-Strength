60b2f264555c6a1f34a905873d4ce865520be02b
aps124 <aps124@ic.ac.uk>
2024-10-30 16:10:24 +0000

    fixing comments

--- prune.py
+++ prune.py
@@ -3,19 +3,24 @@ from evaluate import simple_compute_accuracy
 
 
 def prune_tree(tree, X_test, Y_test):
-    """ Prunes the tree by iteratively replacing nodes with child nodes to maximize test accuracy """
+    """ Given a trained tree and test data, prune the tree to maximize accuracy
+    """
+
+    # Find all nodes directly connected to two leaves
     nodes_with_two_leaves = find_nodes_with_two_leaves(tree, [])
-    nodes_visited = set()
+    nodes_visited = set()  # Store nodes already evaluated
     baseline_accuracy = simple_compute_accuracy(tree, X_test, Y_test)
 
     while nodes_with_two_leaves:
-        node = nodes_with_two_leaves.pop()
+        node = nodes_with_two_leaves.pop()  # Get node to evaluate
 
         if node in nodes_visited:
             continue
         nodes_visited.add(node)
 
-        # Evaluate both left and right prunings
+        # Copy tree so that pruning can be evaluated and reverted
+        # Left tree will replace the node by the left child and
+        # Right tree will replace the node by the right child
         tmp_tree_left = copy.deepcopy(tree)
         tmp_tree_right = copy.deepcopy(tree)
         replace_node(tmp_tree_left, node, use_right=False)
@@ -27,10 +32,13 @@ def prune_tree(tree, X_test, Y_test):
 
         # Update tree if accuracy improves
         if left_accuracy >= baseline_accuracy or right_accuracy >= baseline_accuracy:
+
+            # Left accuracy is the highest and therefore left tree is optimal
             if left_accuracy >= right_accuracy:
                 tree = tmp_tree_left
                 baseline_accuracy = left_accuracy
             else:
+                # Right accuracy is the highest and therefore right tree is optimal
                 tree = tmp_tree_right
                 baseline_accuracy = right_accuracy
 
@@ -52,10 +60,17 @@ def find_nodes_with_two_leaves(tree, matching_nodes):
     return matching_nodes
 
 def replace_node(tree, node, use_right=True):
-    """ Replaces the specified node with its left or right child """
+    """ Given a tree and a node, find that node in the tree and replace it with
+    it's child. If use_right, replace with the right child, else replace with left child
+    """
+
     node_feature, node_value = node
+
+    # Current node is a leaf
     if tree['feature'] is None:
         return
+    
+    # Current node is a match and validate that it is connected to two leaves
     if (tree['feature'] == node_feature and tree['value'] == node_value 
         and tree['left']['feature'] is None and tree['right']['feature'] is None):
         
@@ -65,5 +80,7 @@ def replace_node(tree, node, use_right=True):
         tree['left'] = replacement['left']
         tree['right'] = replacement['right']
         return
+
+    # Recursively call on left and right sub-trees
     replace_node(tree['left'], node, use_right)
     replace_node(tree['right'], node, use_right)
